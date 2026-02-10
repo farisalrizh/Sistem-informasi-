530e6864a4ac204ce1dce6c513767ff1b230c8d9
alriz <farisalrizh@gmail.com>
2026-02-10 11:47:55 +0700

    first commit

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("loginForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-
-    let role = document.getElementById("role").value;
-    let user = document.getElementById("username").value;
-
-    alert("Login sebagai: " + role + "\nUsername: " + user);
-});
